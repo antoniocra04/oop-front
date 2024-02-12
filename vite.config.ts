73481149d477b805143a02054809c256ebeb4834
antoniocra04 <antoniocra04@gmail.com>
2024-02-12 19:07:53 +0700

    feature: add aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve:{
+    alias:{
+      '@constants': path.resolve(__dirname, './src/constants'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@store': path.resolve(__dirname, './src/store'),
+			'@ui': path.resolve(__dirname, './src/ui'),
+			'@api': path.resolve(__dirname, './src/api'),
+    }
+  }
 })
